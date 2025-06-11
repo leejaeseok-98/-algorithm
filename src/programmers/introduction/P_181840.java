f5f7434e03f2963d1db3b1d8dc197330061125cd
leejaeseok-98 <wotjrdl98@naver.com>
2025-06-11 18:30:08 +0900

    ㅇㄹㅇㄹ

---
+++ src/programmers/introduction/P_181840.java
@@ -0,0 +1,22 @@
+package programmers.introduction;
+
+public class P_181840 {
+    public static void main(String[] args) {
+        int[] num_list = {1,2,3,4,5};
+        int n = 3;
+        Solution33 sol = new Solution33();
+        System.out.println(sol.solution(num_list, n));
+    }
+}
+class Solution33 {
+    public int solution(int[] num_list,int n) {
+        int answer = 0;
+        for (int i = 0; i < num_list.length; i++) {
+            if (num_list[i] == n) {
+                answer++;
+                break;
+            }
+        }
+        return answer;
+    }
+}
