3ae0bbf167f9715bf5209d78b8d42fe5e5819465
leejaeseok-98 <wotjrdl98@naver.com>
2025-06-20 14:25:57 +0900

    무작위의 k개의 수 뽑기

--- src/programmers/introduction/P_181858.java
+++ src/programmers/introduction/P_181858.java
@@ -1,11 +1,10 @@
 package programmers.introduction;
 
-import java.util.Arrays;
-import java.util.Random;
+import java.util.*;
 
 public class P_181858 {
     public static void main(String[] args) {
-        int[] arr = {0,1,1,2,2,3};
+        int[] arr = {0, 1, 1, 2, 2, 3};
         int k = 3;
         Solution49 sol = new Solution49();
         System.out.println(Arrays.toString(sol.solution(arr, k)));
@@ -13,11 +12,26 @@ public class P_181858 {
 }
 class Solution49 {
     public int[] solution(int[] arr, int k) {
-        Random r = new Random();
-        int[] answer = new int[k];
-        int randomIndex = r.nextInt(arr.length);
-        while (true) {
-            if ()
+        List<Integer> list = new ArrayList<>();
+        Set<Integer> seen = new HashSet<>();
+
+        for (int num : arr) {
+            if (!seen.contains(num)) {
+                list.add(num);
+                seen.add(num);
+            }
+            if (list.size() == k) break;
         }
+
+        while (list.size() < k) {
+            list.add(-1);
+        }
+
+        int[] answer = new int[k];
+        for (int i = 0; i < k; i++) {
+            answer[i] = list.get(i);
+        }
+
+        return answer;
     }
 }
