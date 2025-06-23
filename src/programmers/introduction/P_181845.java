e6ccffa6bb5f8c56a28cc4f1ae7ae78277d868d2
leejaeseok-98 <wotjrdl98@naver.com>
2025-06-23 20:35:47 +0900

    문자열로 변환

---
+++ src/programmers/introduction/P_181845.java
@@ -0,0 +1,16 @@
+package programmers.introduction;
+
+public class P_181845 {
+    public static void main(String[] args) {
+        int n = 123;
+        Sol4 sol4 = new Sol4();
+        System.out.println(sol4.solution(n));
+    }
+}
+class Sol4{
+    public String solution(int n){
+        String answer = "";
+        answer += n;
+        return answer;
+    }
+}
