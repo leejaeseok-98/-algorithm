fbb6fc74b314ea6307da32048141cc15455ea6a3
leejaeseok-98 <wotjrdl98@naver.com>
2025-06-24 19:38:59 +0900

    정수를 나선형으로 배치하기

---
+++ src/programmers/introduction/P_181832.java
@@ -0,0 +1,16 @@
+package programmers.introduction;
+
+import java.util.Arrays;
+
+public class P_181832 {
+    public static void main(String[] args) {
+        int n = 4;
+        Sol13 sol = new Sol13();
+        System.out.println(Arrays.deepToString(sol.solution(n)));
+    }
+}
+class Sol13{
+    public int[][] solution(int n){
+        
+    }
+}
