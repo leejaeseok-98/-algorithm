a33d92941ae312307a4be237f0207b5ee91d5e1e
leejaeseok-98 <wotjrdl98@naver.com>
2025-07-08 14:37:54 +0900

    a+b

---
+++ src/backJoon/noLevel/B_1991.java
@@ -0,0 +1,13 @@
+package backJoon.noLevel;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B_1991 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+    }
+}
