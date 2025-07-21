b9c0e51419e8de2f65181ca09a9347a44ad089d3
leejaeseok-98 <wotjrdl98@naver.com>
2025-07-21 11:24:46 +0900

    백준10844

--- src/backJoon/silver/B_9095.java
+++ src/backJoon/silver/B_9095.java
@@ -1,7 +1,14 @@
-package backJoon.silver;
-
-public class B_9095 {
-    public static void main(String[] args) {
-
-    }
-}
+//package backJoon.silver;
+//
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//
+//public class B_9095 {
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        int n = Integer.parseInt(br.readLine());
+//
+//
+//    }
+//}
