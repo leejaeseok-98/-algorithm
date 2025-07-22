9107464d3f6f67dcc711d9b7f3b9202afe67d5f4
leejaeseok-98 <wotjrdl98@naver.com>
2025-07-22 09:46:12 +0900

    이친수

---
+++ src/backJoon/silver/B_2193.java
@@ -0,0 +1,27 @@
+package backJoon.silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B_2193 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        long [] answer = new long[N+1]; // 정수형 범위를 초과하는 것에 주의한다.
+
+        answer[0] = 0;
+        answer[1] = 1;
+
+
+
+        for( int i =2; i<=N; i++){
+            answer[i] = answer[i-1]+answer[i-2];
+        }
+
+        System.out.println(answer[N]);
+
+    }
+}
