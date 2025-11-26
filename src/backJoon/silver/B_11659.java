cd8b70d53f8e6961eb200001cf6383546be4f781
leejaeseok-98 <wotjrdl98@naver.com>
2025-11-26 11:48:10 +0900

    코테 2일차 구간합

--- src/backJoon/silver/B_11659.java
+++ src/backJoon/silver/B_11659.java
@@ -11,10 +11,22 @@ public class B_11659 {
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int totalElements = Integer.parseInt(st.nextToken());
         int totalLoopNum = Integer.parseInt(st.nextToken());
+        int[] arr = new int[totalElements+1];
 
         StringTokenizer arrSt = new StringTokenizer(br.readLine(), " ");
+        for (int i = 1; i <= totalElements;i++){
+            arr[i] = Integer.parseInt(arrSt.nextToken());
+        }
+        for (int i = 2; i <= totalElements;i++){
+            arr[i] += arr[i-1];
+        }
 
-
+        for (int i = 0; i < totalLoopNum; i++){
+            StringTokenizer st2 = new StringTokenizer(br.readLine(), " ");
+            int start = Integer.parseInt(st2.nextToken());
+            int end = Integer.parseInt(st2.nextToken());
+            System.out.println(arr[end] - arr[start-1]);
+        }
 
     }
 }
