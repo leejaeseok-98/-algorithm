cc9cfa0c27d78b23b360fcfa8765a006ddcb6a2a
leejaeseok-98 <wotjrdl98@naver.com>
2025-11-26 11:55:59 +0900

    코테 2일차 구간합 수정

--- src/backJoon/gold/B_10986.java
+++ src/backJoon/gold/B_10986.java
@@ -14,7 +14,7 @@ public class B_10986 {
         long cnt = 0;
 
         long[] s = new long[N];
-        long[] c = new long[N];
+        long[] c = new long[M];
 
         st = new StringTokenizer(br.readLine());
         s[0] = Integer.parseInt(st.nextToken());
@@ -27,7 +27,7 @@ public class B_10986 {
             if (remainder == 0) cnt++;
             c[remainder]++;
         }
-        for (int i = 0; i < N;i++){
+        for (int i = 0; i < M;i++){
             if (c[i] > 1){
                 cnt = cnt + (c[i] * (c[i]-1)/2);
             }
