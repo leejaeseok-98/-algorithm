7df0716a4e773414ebb417f63afd316c187e6646
leejaeseok-98 <wotjrdl98@naver.com>
2026-01-09 18:34:33 +0900

    설탕공장

---
+++ src/backJoon/silver/v4/B_2839.java
@@ -0,0 +1,28 @@
+package backJoon.silver.v4;
+
+import java.util.Scanner;
+
+public class B_2839 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int cnt = 0;
+
+        while (n > 2) {
+            if (n % 5 == 0){
+                cnt += n / 5;
+                System.out.println(cnt);
+                return;
+            }
+            n -= 3;
+            cnt++;
+        }
+        if (n == 1){
+            System.out.println(-1);
+        } else {
+            System.out.println(cnt);
+        }
+
+    }
+}
---
+++ src/backJoon/silver/v5/B_11650.java
@@ -0,0 +1,24 @@
+package backJoon.silver.v5;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class B_11650 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] arr = new int[n][2];
+        for (int i = 0; i < n; i++){
+            arr[i][0] = sc.nextInt();
+            arr[i][1] = sc.nextInt();
+        }
+        Arrays.sort(arr, (e1,e2) -> {
+            if (e1[0] == e2[0]) {
+                return e1[1] - e2[1];
+            } else {
+                return e1[0] - e2[0];
+            }
+        });
+        }
+    }
