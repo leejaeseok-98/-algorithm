7dc1ca2e4d7e059c3df4b76e58a74b2a5c49116a
leejaeseok-98 <wotjrdl98@naver.com>
2026-01-23 14:08:18 +0900

    파도반수열

---
+++ src/backJoon/silver/v3/B_9461.java
@@ -0,0 +1,23 @@
+package backJoon.silver.v3;
+
+import java.util.Scanner;
+
+public class B_9461 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        long[] dp = new long[101];
+        dp[1] = 1;
+        dp[2] = 1;
+        dp[3] = 1;
+
+        for (int i = 4; i <= 100; i++) {
+            dp[i] = dp[i - 3] + dp[i - 2];
+        }
+
+        for (int i = 0; i < t; i++) {
+            int n = sc.nextInt();
+            System.out.println(dp[n]);
+        }
+    }
+}
